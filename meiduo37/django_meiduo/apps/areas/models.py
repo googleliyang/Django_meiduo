a401d58f8aaed3fafe0e78df27df1ee8b4e9d0f1
ubuntu <ubuntu@gmail.com>
2019-02-27 18:53:16 +0800

    add area model

--- meiduo37/django_meiduo/apps/areas/models.py
+++ meiduo37/django_meiduo/apps/areas/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Area(models.Model):
+    name = models.CharField(max_length=10)
+    parent = models.ForeignKey('self', null=True, blank=True)
+    # 1 自关联一对多, 观察书库表结构
+    # 2 插省市区数据入表, 插入几条就好了
+    # 3 设定url地址编写，查询结果, 无参为省，/市 /市/区
+
+    class Meta:
+        db_table = 'areas'
+        verbose_name = '区域'
+        verbose_name_plural = verbose_name
